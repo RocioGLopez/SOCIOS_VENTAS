2e7c917ea5860e5fdb2a53c73bbf164e786e116c
Kesdy10 <kesdy.01@gmail.com>
2026-04-20 21:53:20 -0600

    comiit
    Quitar error

--- ERPCompras/Backend/app.py
+++ ERPCompras/Backend/app.py
@@ -3,7 +3,6 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
-*/*
 @app.route('/compras/historial')
 def historial():
     inicio = request.args.get('inicio')
